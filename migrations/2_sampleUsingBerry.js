77f9d6714c7218b6dfb9edbc66d8b55baa1fc834
berrydatadev <info@berrydata.co>
2021-01-25 21:54:38 +0800

    read env from .env, no hard code

--- migrations/2_sampleUsingBerry.js
+++ migrations/2_sampleUsingBerry.js
@@ -1,17 +1,7 @@
-const Migrations = artifacts.require("Migrations");
 const SampleUsingBerry = artifacts.require("SampleUsingBerry");
-
-// TODO add bsc and testnet berry master address
-const bscAddress = "";
-const testAddress = "";
-const localAddress = "0x7DdC408C0Cd13D3543156AE2bc5772C56E91AA0f";
+require("dotenv").config();
+const masterAddress = process.env.MASTER;
 
 module.exports = function (deployer, network) {
-  if (network === "bsc") {
-    deployer.deploy(SampleUsingBerry, bscAddress);
-  } else if (network === 'testnet') {
-    deployer.deploy(SampleUsingBerry, testAddress);
-  } else {
-    deployer.deploy(SampleUsingBerry, localAddress);
-  }
+  deployer.deploy(SampleUsingBerry, masterAddress);
 };
