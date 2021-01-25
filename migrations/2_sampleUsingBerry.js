812773f03442824e9c738878cdf5e3bed0fe55bb
berrydatadev <info@berrydata.co>
2021-01-25 19:09:30 +0800

    add local network

--- migrations/2_sampleUsingBerry.js
+++ migrations/2_sampleUsingBerry.js
@@ -1,13 +1,17 @@
 const Migrations = artifacts.require("Migrations");
 const SampleUsingBerry = artifacts.require("SampleUsingBerry");
 
-const mainnetAddress = "";
-const testAddress = "0x7DdC408C0Cd13D3543156AE2bc5772C56E91AA0f";
+// TODO add bsc and testnet berry master address
+const bscAddress = "";
+const testAddress = "";
+const localAddress = "0x7DdC408C0Cd13D3543156AE2bc5772C56E91AA0f";
 
 module.exports = function (deployer, network) {
-  if (network == "bsc") {
-    deployer.deploy(SampleUsingBerry, mainnetAddress);
-  } else {
+  if (network === "bsc") {
+    deployer.deploy(SampleUsingBerry, bscAddress);
+  } else if (network === 'testnet') {
     deployer.deploy(SampleUsingBerry, testAddress);
+  } else {
+    deployer.deploy(SampleUsingBerry, localAddress);
   }
 };
